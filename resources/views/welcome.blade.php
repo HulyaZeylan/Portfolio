c1ac8d52d6342d4c32106f1ffde5bde3028f645a
HulyaZ <zeylan.hulya@gmail.com>
2021-02-03 22:40:13 +0300

    1st commit - Project Set-Up

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <link id="callCss" rel="stylesheet" href="{{ asset('bootstrap.min.css') }} " media="screen"/>
+    <link href=" {{asset('themes/css/try.css')}}" rel="stylesheet" media="screen"/>
+
+    <title>Hello</title>
+</head>
+<body>
+
+<h1>fssfsdf</h1>
+
+
+
+<button id="name" type="button">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+
+
+</body>
+</html>
